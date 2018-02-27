d26d2b41f60a1fc748104c0c92fb55d7fe44c4d2
Josef Biehler <jbiehler@samhammer.de>
2018-02-27 11:00:07 +0100

    add possibility to finalize user input

--- WebApplication1/WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -17,11 +17,13 @@ namespace WebApplication1.Controllers
         public WatsonAResponse Query([FromBody]WatsonCQuery query)
         {
             var data = StaticStorage.GetData<TempData>(query.SessionId);
+            // wir müsen zuerst prüfen, ob der ankommende text ein neuer Intent ist
             var result = new WatsonProvider().Query(query.Text, null, null);
 
             if (data != null && result.intents.Max(x => x.confidence) > 0.7 && data.Intent != result.intents.OrderByDescending(x => x.confidence).First().intent)
             {
                 // es gab einen vorherigen intent und der neue intent ist anders + signifikant
+                // lösche den alten Intent
                 StaticStorage.AddData(query.SessionId, null);
                 data = null;
             }
@@ -29,29 +31,35 @@ namespace WebApplication1.Controllers
             {
                 // es gab keinen vorherigen intent
                 data = new TempData();
-
-                
             }
             else
             {
-                // es gab einen vorherigen intent und der neue ist nicht signifikant
+                // es gab einen vorherigen intent und der neue ist nicht signifikant oder der neue ist der gleiche wie der alte oder der neue ist nicht existent
+                // dann frage nochmal watson ab, jetzt aber mit den temporären daten
                 result = new WatsonProvider().Query(query.Text, new List<WatsonAIntent>{new WatsonAIntent{intent = data.Intent, confidence = 1 } }, data.Entities);
+
+                // im idealfall wurden mit der Usereingabe bestimmte entitäten befüllt und im dialogflow wurde ein step vorangeschritten
                 if (result.entities.Count > 0)
                 {
+                    // dann speichere die angaben für später
                     data.Entities.AddRange(result.entities);
-                    result = new WatsonProvider().Query(query.Text, new List<WatsonAIntent> { new WatsonAIntent { intent = data.Intent, confidence = 1} }, data.Entities);
+                    // für was habe ich diese zeule eingebaut?? result = new WatsonProvider().Query(query.Text, new List<WatsonAIntent> { new WatsonAIntent { intent = data.Intent, confidence = 1} }, data.Entities);
                 }
             }
 
+            // baue ein neues storage objekt, welches den intent und die entitäten (später = context) beinhaltet
             var newData = new TempData
             {
                 Intent = data?.Intent ?? result.intents.OrderByDescending(x => x.confidence).First().intent,
                 Entities = result.entities ?? new List<WatsonAEntity>()
             };
-            newData.Entities.AddRange(result.entities);
-            newData.Entities = newData.Entities.Distinct().ToList();
+
+            //newData.Entities.AddRange(result.entities);
+            //newData.Entities = newData.Entities.Distinct().ToList();
             StaticStorage.AddData(query.SessionId, newData);
 
+            // jetzt könnte folgendes passieren;:
+            // a) wir haben einen spezielle intent welcher eine Validierung benötigt oder eine Serverseitige Aktion -> Schnittstelle für Wissensmanager
             if (result.intents[0].intent == "math.request.binary2")
             {
                 if (Calculator.IsValid(result.input.text))
@@ -80,13 +88,15 @@ namespace WebApplication1.Controllers
                     
                     newData.Entities.Add(new WatsonAEntity { entity = "operator", value = op.ToString() });
 
-                    newData.Entities.Add(new WatsonAEntity{entity = "sessionId", value=new Random().NextDouble().ToString()});
+                    StaticStorage.AddData(query.SessionId, null);
+                    
 
 
                     result = new WatsonProvider().Query(query.Text, new List<WatsonAIntent> { new WatsonAIntent { intent = newData.Intent, confidence = 1 } }, newData.Entities);
                 }
                 else
                 {
+                    StaticStorage.AddData(query.SessionId, null);
                     var context = new List<WatsonAEntity>(newData.Entities);
                     context.Add(new WatsonAEntity {entity = "math_request_failed_right", value = "true"});
                     var items = Calculator.Extract(result.input.text);
@@ -94,6 +104,21 @@ namespace WebApplication1.Controllers
                     newData.Entities.Add(new WatsonAEntity { entity = "right", value = items.Item3.ToString() });
                     newData.Entities.Add(new WatsonAEntity { entity = "operator", value = items.Item2 });
                     result = new WatsonProvider().Query(query.Text, new List<WatsonAIntent> { new WatsonAIntent { intent = newData.Intent, confidence = 1 } }, context);
+                }
+            }
+            // b) der schritt im dialogflow wurde mit "final" gekennzeichnet, was für uns heißt: Schließe diese Konversation ab ( = Lösche Daten) und führe eine Aktion durch
+            //      Diese Aktion ist im idealfall im context im dialogflow spezifiziert und abhängig vom intent
+            if (result.intents[0].intent == "math.request.binary" && result.context.ContainsKey("final"))
+            {
+                StaticStorage.AddData(query.SessionId, null);
+                // Der Wissensmanager möchte, dass nun dieser Intent beendet ist. Wir führen die von ihmn spezifizierte Aktion durch. Mögliche Werte: "createLinkBasedOnContext" oder "createSolutionBasedOnCo´ntext"
+                if (result.context["userResponse"].ToString() == "createLink")
+                {
+                    result.output.text = new List<string>{"Hier finden Sie die Lösung für ihr Problem <a href='http://google.de?q=BlaBlub'>Lösung</a>"};
+                }
+                else
+                {
+                    result.output.text = new List<string> { "Machen Sie dies und jenes um ihr Passwort zurückzusetzen" };
                 }
             }
             return result;
